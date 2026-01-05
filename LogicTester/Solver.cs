2196f5a2132a910a81f7ca69ef412c797ffa8bad
Nikolas-Ioannis Giannakopoulos <nikolasgiannhs@gmail.com>
2026-01-05 09:47:33 +0200

    [Update] Finished the backtracking algorithm

--- LogicTester/Solver.cs
+++ LogicTester/Solver.cs
@@ -6,53 +6,79 @@ namespace LogicTester
 
         public void Solve(Cube cube)
         {
-            for(int faceIndex = 0; faceIndex < 6; faceIndex++)
-            {
-                for(int row = 0; row < 9; row++)
-                {
-                    for(int collumn = 0; collumn < 9; collumn++)
-                    {
-                        Cell currentCell = cube.cellList[faceIndex, row, collumn];
-                        if(currentCell.getNumber() == 0)
-                        {
-                            currentCell.setNumber(1);
-                        }
-                    }
-                }
-            }
+            Cell currentCell = cube.cellList[0, 0, 0];
+
         }
 
+        public bool run(Cube cube, Cell cell)
+        {
+            for(int number = 1; number < 10; number++)
+            {
+                cell.setNumber(number);
+                if(Checker(cell, cube))
+                {
+                    CellPosition position = cell.getPosition();
+                    var column = (position.column + 1) % 3;
+                    int row = position.row;
+                    int face = (int)position.face;
+                    if(column==0)
+                    {
+                        row = (row +1) % 3; 
+                        if(row == 0)
+                        {
+                            face = (int)position.face + 1;
+                            if(face == 6)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    Cell nextCell = cube.GetCell(new CellPosition((CubeFaces)face, row, column));
+                    if (run(cube, nextCell) == true)
+                    {
+                        return true;
+                    }
+                }          
+            }
+            cell.setNumber(0);
+            return false;
+        }
 
         public bool Checker(Cell cell, Cube cube)
         {
-            //TO-DO: CHECK SUDOKU
-            if(cell.getPosition().row == 1 && cell.getPosition().column == 1)
-            {
-                return true;
-            }
-            else
-            {
-                bool flag = false;
-                int[,] edges = new int[4,2]{ {0,0},{1,0}, {1,2}, {2,1}};
-                for (int i = 0; i < edges.GetLength(0); i++) {
-                    if (cell.getPosition().row == edges[i,0] && cell.getPosition().column == edges[i,1])
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag)
+            if(CheckFace(cube, cell)){
+                if(cell.getPosition().row == 1 && cell.getPosition().column == 1)   
                 {
-                    return CheckEdges(cube);
+                    return true;
                 }
                 else
                 {
-                    return CheckCorners(cube);
+                    bool flag = false;
+                    int[,] edges = new int[4,2]{ {0,0},{1,0}, {1,2}, {2,1}};
+                    for (int i = 0; i < edges.GetLength(0); i++) {
+                        if (cell.getPosition().row == edges[i,0] && cell.getPosition().column == edges[i,1])
+                        {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        return CheckEdges(cube);
+                    }
+                    else
+                    {
+                        return CheckCorners(cube);
+                    }
                 }
+            }
+            else
+            {
+                return false;
             }
         }
 
-        }
+        
         public bool CheckEdges(Cube cube)
         {
             foreach (var edgePair in CubeTopology.Edges)
